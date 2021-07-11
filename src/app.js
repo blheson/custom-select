310ec0efeb1c6947da27bc6a0520ae99abac6b61
blheson <blessingworld007@gmail.com>
2021-07-11 01:32:53 +0100

    add css overwrite

--- src/app.js
+++ src/app.js
@@ -1,48 +1,105 @@
+
 const customSelect = {
     index: 1,
-    createList: (arr) => {
-        let opt = '';
+    coreSelect: null,
+    cache: [],
+    createList: function (arr) {
+        let opt = document.createElement('div');
+        opt.classList.add('option-box')
         arr.forEach(e => {
-            opt += `<div class="sing-opt" data-value="${e[0]}">${e[1]}</div>`
+            opt.appendChild(this.optionList(e))
         })
         return opt
     },
-    cache: [],
-    parseData: function (singSelect, options = null) {
+    inputListen: function (customS, e) {
+        let opt = document.createElement('div');
+        opt.classList.add('option-box')
+        let search = e.target.value ?? ''
+        this.cache[customS.dataset.value].forEach(sing => {
+            if (sing[1].toLowerCase().includes(search.toLowerCase()))
+                opt.appendChild(this.optionList(sing));
+        });
+        return opt;
+    },
+    optionList: (data) => {
+        let div = document.createElement('div')
+        div.classList.add('option-list')
+        div.dataset.value = data[0];
+        div.innerText = data[1];
+        return div;
+    },
+    parseData: function (singSelect) {
         singSelect.querySelectorAll('option').forEach(option => {
-
-
             this.cache[this.index].push([option.value, option.innerText])//ish
         });
         singSelect.style.display = 'none'
     },
-    createBoundingBox: function () {
+    createInput: () => {
+        let input = document.createElement('input')
+        input.name = 'custom-search'
+        input.type = 'search';
+        return input
+    },
+    createOptionBox: function () {
+        let optionBox = document.createElement('div');
+        optionBox.classList.add('custom-option');
+        optionBox.style['display'] = 'none';
+        let input = this.createInput()
+        optionBox.appendChild(input)
         let opt = this.createList(this.cache[this.index])
-        let optionBox = `<div class="customOption" style="display:none"><input type="search" name="customSearch">${opt}</div>`;
+        optionBox.appendChild(opt)
+        return optionBox
+    },
+    createClickPad: function () {
+        let clickDiv = document.createElement('div')
+        clickDiv.innerText = 'Select option';
+        clickDiv.classList.add('click-to-show')
+        return clickDiv;
+    },
+    createOverallBox: function () {
         let customS = document.createElement('div')
+        customS.dataset.value = this.index
         customS.classList.add('big-bound-box' + this.index)
-        customS.innerHTML = `<div class="clicktoshow">Select option</div>${optionBox}`
+        return customS;
+    },
+    createBoundingBox: function () {
+        let optionBox = this.createOptionBox()
+        let customS = this.createOverallBox()
+        let clickDiv = this.createClickPad()
+        customS.appendChild(clickDiv);
+        customS.appendChild(optionBox);
         return customS;
     },
     listenForClick: function (customS) {
         customS.addEventListener('click', e => {
-            if (e.target.classList.contains('clicktoshow')) {
-                let optionBox = customS.querySelector('.customOption');
-                optionBox.style.display = optionBox.style.display == 'none' ?
-                    '' : 'none'
+            let customBox = customS.querySelector('.custom-option');
+            if (e.target.classList.contains('click-to-show')) {
+                customBox.style.display = customBox.style.display == 'none' ? '' : 'none'
             }
-            if (e.target.classList.contains('sing-opt')) {
-                // console.log(this.coreSelect)
+            if (e.target.classList.contains('option-list')) {
                 this.coreSelect.value = e.target.dataset.value
-                customS.querySelector('.clicktoshow').innerText = e.target.innerText
+                customS.querySelector('.click-to-show').innerText = e.target.innerText
+                customBox.style.display = 'none'
+            }
+            if (e.target.name == 'custom-search') {
+                e.target.addEventListener('input', () => {
+                    let opt = this.inputListen(customS, e);
+                    if (customS.querySelector('.option-box') != null)
+                        customS.querySelector('.option-box').remove()
+                    customBox.appendChild(opt);
+                })
             }
         })
     },
-    coreSelect: null,
-    init: function (status) {
-        if (!status) {
-            return
-        }
+    addStyle: function (option) {
+        let style = document.createElement('style')
+
+        let cssText = option.overwrite ? option.content : ".custom-box {width: 500px;display: flex;justify-content: center;margin: auto;align-items: center;align-content: center}.click-to-show {cursor: pointer;text-align: center;background: #509696;padding: 1rem;width: 209px}.custom-option {padding: 1rem;border-radius: 5px;background: #ddd;position: absolute;width: 209px;}.option-box>div {padding: 0.7rem 0;border-bottom: 1px solid #bfbfbf;font-size: 14px;cursor: pointer}.option-box>div:hover {border-bottom: 1px solid #8d8d8d}input {padding: 1rem;border: none}" + option.content
+        style.innerText = cssText
+        document.head.appendChild(style);
+    },
+    init: function (option = false) {
+        this.addStyle(option)
         document.querySelectorAll('select[name=custom]').forEach(singSelect => {
             this.coreSelect = singSelect
             this.cache[this.index] = [];
@@ -52,6 +109,5 @@ const customSelect = {
             singSelect.parentElement.appendChild(customS)
             this.index++
         })
-
     }
 }
